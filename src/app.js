3afa31ea21195cc5fd2b86b80c6a62229b1f6888
Francisca Bahamondes <frbahamondes@gmail.com>
2025-02-09 13:18:46 -0300

    Configurar Handlebars

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
 const app = express();
+
+// Configurar Handlebars como motor de plantillas
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware para manejar JSON
 app.use(express.json());
@@ -11,6 +19,11 @@ const cartsRouter = require('./routes/carts.router');
 // Rutas base
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+
+// Ruta para renderizar la vista home
+app.get('/', (req, res) => {
+    res.render('home'); // Renderiza home.handlebars
+});
 
 // Configuración del puerto
 const PORT = 8080;
