583a5910685b1be10aba478b07242010ec53d915
Francisca Bahamondes <frbahamondes@gmail.com>
2025-08-07 21:56:22 -0400

    CREAR PRODUCT REPOSITORY

---
+++ src/repository/product.repository.js
@@ -0,0 +1,25 @@
+export default class ProductRepository {
+    constructor(dao) {
+        this.dao = dao;
+    }
+
+    getProducts = async (filters) => {
+        return this.dao.getAll(filters);
+    };
+
+    getProductById = async (id) => {
+        return this.dao.getById(id);
+    };
+
+    createProduct = async (product) => {
+        return this.dao.create(product);
+    };
+
+    updateProduct = async (id, data) => {
+        return this.dao.update(id, data);
+    };
+
+    deleteProduct = async (id) => {
+        return this.dao.delete(id);
+    };
+}
